791a5e9b2afbce0078c1e158b5e309d24dddac83
wangfan01 <wangfan01@kaikeba.com>
2022-03-10 13:57:41 +0800

    feat: 修改仓库

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,11 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
+
+# 关联远程分支
+git remote add origin https://github.com/songzi0321/songzi.github.io.git
  
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f github.com:songzi0321/songzi.github.io.git master
+git push -f https://github.com/songzi0321/songzi.github.io.git master
  
 cd -
